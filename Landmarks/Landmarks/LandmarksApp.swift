3a203aea54f196f6951ac62e0dcbfe9f89df760e
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-08-13 19:26:20 +0300

    build: create project file

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by CHURILOV DMITRIY on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
