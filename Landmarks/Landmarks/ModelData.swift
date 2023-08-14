da646f22f1a9d908e0f1688062bd348260105614
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-08-14 23:40:38 +0300

    build: write landmark model json parser

---
+++ Landmarks/Landmarks/ModelData.swift
@@ -0,0 +1,30 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by CHURILOV DMITRIY on 14.08.2023.
+//
+
+import Foundation
+
+func load<T: Decodable>(_ filename: String) -> T {
+	let data: Data
+
+	guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+	else {
+		fatalError("Couldn't find \(filename) in main bundle.")
+	}
+
+	do {
+		try data = Data(contentsOf: file)
+	} catch {
+		fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+	}
+
+	do {
+		return try JSONDecoder().decode(T.self, from: data)
+	} catch {
+		fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+	}
+
+}
