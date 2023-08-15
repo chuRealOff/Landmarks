28b6b6b9867633ea722f35c015ae16e4fda52b73
Dmitry Churilov <98746743+chuRealOff@users.noreply.github.com>
2023-08-15 10:48:18 +0300

    refactor: replace hardcode in the body

--- Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -8,36 +8,44 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
-    var body: some View {
-		VStack {
-			MapView()
-				.ignoresSafeArea(edges: .top)
+	var landmark: Landmark
+
+	var body: some View {
+		ScrollView {
+			MapView(coordinate: landmark.locationCoordinate)
 				.frame(height: 300)
 
-			CircleImage()
+			CircleImage(image: landmark.image)
 				.offset(y: -130)
 				.padding(.bottom, -130)
 
 			VStack(alignment: .leading) {
-				Text("Turtle Rock")
+				Text(landmark.name)
 					.font(.title)
 				HStack {
-					Text("Joshua Tree National Park")
+					Text(landmark.park)
 					Spacer()
-					Text("California")
+					Text(landmark.state)
 				}
 				.font(.subheadline)
 				.foregroundColor(.secondary)
+
+				Divider()
+
+				Text("About \(landmark.name)")
+					.font(.title2)
+				Text(landmark.description)
 			}
 			.padding()
-
-			Spacer()
 		}
-    }
+		.ignoresSafeArea(edges: .top)
+		.navigationTitle(landmark.name)
+		.navigationBarTitleDisplayMode(.inline)
+	}
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
-    static var previews: some View {
-        LandmarkDetail()
-    }
+	static var previews: some View {
+		LandmarkDetail(landmark: landmarks[0])
+	}
 }
